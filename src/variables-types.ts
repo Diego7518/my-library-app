772d8361611f34d1303bdb718bbf96b992814045
Diego7518 <hffjdkksfk835@gmail.com>
2024-06-13 07:38:52 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,38 @@
+let bookTitle: string = "TypeScript Basics";
+const libraryName: string = "My Library";
+let numberOfBooks: number = 100;
+let isLibraryOpen: boolean = true;
+
+let titulo: string;
+let autor: string;
+let genero: string;
+let idioma: string;
+let precio: string;
+let formato: string;
+let isbn: string;
+let descripcion: string;
+let estado: string;
+let ubicacion: string;
+let fecha_publicacion: Date; 
+let editorial: string;
+let paginas: number;
+let dimensiones: { ancho: number; profundidad: number; altura: number }; 
+let peso: string;
+let disponible: boolean;
+
+titulo = "Cuchilla";
+autor = "Evelio Rosero";
+genero = "Ficción";
+idioma = "Español";
+precio = "29.99 USD";
+formato = "Físico";
+isbn = "9789580009948";
+descripcion = "Unos gemelos tienen de profesor a cuchilla, un profesor muy temido por todo el colegio el cual esconde un secreto.";
+estado = "Nuevo";
+ubicacion = "Colombia";
+fecha_publicacion = new Date("2000-06-01");
+editorial = "Torre de papel";
+paginas = 152;
+dimensiones = { ancho: 15, profundidad: 2.5, altura: 23 };
+peso = "500g"; 
+disponible = true;
